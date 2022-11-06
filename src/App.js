6e734801d99796735c9fe2ce49d77d2736c62458
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-06 16:49:47 +0000

    Event Listeners Added
    Simple arrow functions linking the buttons to a console log.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
 import "./App.css"
 
-function App() { 
+function App() {
+  
+  const firstNumPressed = (event) => {
+    console.log(event.target.innerText);
+  }
+
+  const signButtonPressed = (event) => {
+    console.log(event.target.innerText);
+  }
+
+  const secondNumPressed = (event) => {
+    console.log(event.target.innerText);
+  }
+
+  const equalButtonPressed = (event) => {
+    console.log(event.target.innerText)
+  }
 
   return (
     <div className="calculator">
         <div className="panel">
           <p>0</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={firstNumPressed}>1</button>
+            <button onClick={firstNumPressed}>2</button>
+            <button onClick={firstNumPressed}>3</button>
+            <button onClick={firstNumPressed}>4</button>
+            <button onClick={firstNumPressed}>5</button>
+            <button onClick={firstNumPressed}>6</button>
+            <button onClick={firstNumPressed}>7</button>
+            <button onClick={firstNumPressed}>8</button>
+            <button onClick={firstNumPressed}>9</button>
+            <button onClick={firstNumPressed}>0</button>
+            <button onClick={firstNumPressed}>Clear</button>
           </div>
         </div>
         
         <div className="panel">
           <p>+</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={signButtonPressed}>+</button>
+            <button onClick={signButtonPressed}>-</button>
+            <button onClick={signButtonPressed}>*</button>
+            <button onClick={signButtonPressed}>÷</button>
           </div>
         </div>
 
         <div className="panel">
           <p>0</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={secondNumPressed}>1</button>
+            <button onClick={secondNumPressed}>2</button>
+            <button onClick={secondNumPressed}>3</button>
+            <button onClick={secondNumPressed}>4</button>
+            <button onClick={secondNumPressed}>5</button>
+            <button onClick={secondNumPressed}>6</button>
+            <button onClick={secondNumPressed}>7</button>
+            <button onClick={secondNumPressed}>8</button>
+            <button onClick={secondNumPressed}>9</button>
+            <button onClick={secondNumPressed}>0</button>
+            <button onClick={secondNumPressed}>Clear</button>
           </div>
         </div>
         <div className="panel answer">
           <p>0</p>
           <div>
-            <button>=</button>
+            <button onClick={equalButtonPressed}>=</button>
           </div>
         </div>
     </div>
